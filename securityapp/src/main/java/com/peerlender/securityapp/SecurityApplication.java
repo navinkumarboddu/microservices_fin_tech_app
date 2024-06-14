89ce695c72a05948d32ad6d8c34e3c0ff87582c9
Navinkumar Boddu <swarmrobots@gmail.com>
2024-06-14 14:48:00 +0530

    Closes #14 updated code to test Mock User data

--- securityapp/src/main/java/com/peerlender/securityapp/SecurityApplication.java
+++ securityapp/src/main/java/com/peerlender/securityapp/SecurityApplication.java
@@ -1,13 +1,27 @@
 package com.peerlender.securityapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.peerlender.securityapp.user.model.User;
+import com.peerlender.securityapp.user.model.repository.UserRepository;
+
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("Bujji","2898_AD"));
+		userRepository.save(new User("Bhairava","2898_AD"));
 	}
 
 }
--- securityapp/src/main/java/com/peerlender/securityapp/user/model/User.java
+++ securityapp/src/main/java/com/peerlender/securityapp/user/model/User.java
@@ -2,8 +2,10 @@ package com.peerlender.securityapp.user.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "`user`")
 public class User {
 
 	@Id
