144d438e80377cd408b320527b902356b4df5e84
Navinkumar Boddu <swarmrobots@gmail.com>
2024-06-17 18:30:05 +0530

    Closes #18 added User model

--- profile/src/main/java/com/peerlender/profile/domain/model/User.java
+++ profile/src/main/java/com/peerlender/profile/domain/model/User.java
@@ -1,5 +1,6 @@
-package com.peerlender.lendingengine.domain.model;
+package com.peerlender.profile.domain.model;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
@@ -21,15 +22,20 @@ public final class User {
 
 	private String occupation;
 
+	private LocalDate registerSince;
+
 	public User() {
 	}
 
-	public User(String userName, String firstName, String lastName, int age, String occupation) {
+	public User(String userName, String firstName, String lastName, int age, String occupation,
+			LocalDate registerSince) {
+		super();
 		this.userName = userName;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
 		this.occupation = occupation;
+		this.registerSince = registerSince;
 	}
 
 	public String getUserName() {
@@ -52,15 +58,19 @@ public final class User {
 		return occupation;
 	}
 
+	public LocalDate getRegisterSince() {
+		return registerSince;
+	}
+
 	@Override
 	public String toString() {
 		return "User [userName=" + userName + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age
-				+ ", occupation=" + occupation + "]";
+				+ ", occupation=" + occupation + ", registerSince=" + registerSince + "]";
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(age, firstName, lastName, occupation, userName);
+		return Objects.hash(age, firstName, lastName, occupation, registerSince, userName);
 	}
 
 	@Override
@@ -74,7 +84,7 @@ public final class User {
 		User other = (User) obj;
 		return age == other.age && Objects.equals(firstName, other.firstName)
 				&& Objects.equals(lastName, other.lastName) && Objects.equals(occupation, other.occupation)
-				&& Objects.equals(userName, other.userName);
+				&& Objects.equals(registerSince, other.registerSince) && Objects.equals(userName, other.userName);
 	}
 
 }
